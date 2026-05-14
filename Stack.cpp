f8f1b8441c423473af985aef3ab0f5d95ea3872a
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-05-14 23:46:08 +0700

    mengisi fungsi publik pada class stack

--- Stack.cpp
+++ Stack.cpp
@@ -20,4 +20,17 @@ class stack
 private:
     Node *top;
 public:
-   
+    stack()
+    {
+        top = NULL;
+    }
+
+     int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
